083693be32e7b51c79b758f344d1a79a9cdf55f5
Wil Black <wil@pointnineseven.com>
2014-12-17 10:17:11 -0800

    Added correct deep copy of choices array in multi-select.

--- src/viewpoint/question-types/multi-select/directives.js
+++ src/viewpoint/question-types/multi-select/directives.js
@@ -21,7 +21,7 @@ angular.module('p97.questionTypes')
             scope.choicesSelected = 0;
             scope.errors = [];
             scope.valueArray = [];
-            scope.localChoices = scope.question.choices;
+            scope.localChoices =  JSON.parse(JSON.stringify(scope.question.choices));
 
             scope.getContentUrl = function() {
                 if(scope.question.options.templateUrl)
