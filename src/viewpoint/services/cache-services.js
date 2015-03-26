cf207ed2b0e49f4a4cd36d01aa0fb631264dec84
Wil Black <wil@pointnineseven.com>
2015-03-26 10:21:48 -0700

    Adding USE_INDEXED_DB flad.

--- src/viewpoint/services/cache-services.js
+++ src/viewpoint/services/cache-services.js
@@ -14,6 +14,7 @@ angular.module('cache.services', [])
         'media' with a keywords 'filename' and 'data'
 
         */
+        //if (!USE_INDEXED_DB) return;
         var fnames = obj.getFilenames();
         // Cache all geojsonChoices
         _.each(fnames, function(fname){
@@ -46,6 +47,7 @@ angular.module('cache.services', [])
     };
 
     this.getFilenames = function(){
+        
         // Get loop over formstack and get a list of files names to cache
         var fs = $vpApi.getFormstack();
         var fnames = [];
